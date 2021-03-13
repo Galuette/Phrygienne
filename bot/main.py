7e74b8b5d1b5295adac33734965775d922b68b41
Galuette <54235656+Galuette@users.noreply.github.com>
2021-03-13 01:37:58 +0100

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -19,12 +19,12 @@ async def on_message(message):
 
 	if m.startswith ("?info"):
 		if wikipedia.suggest(m[5:len(m)]) != None :
-			await message.channel.send(wikipedia.summary(wikipedia.suggest(m[5:len(m)])))
+			result = wikipedia.summary(wikipedia.suggest(m[5:len(m)])))
 		elif len(wikipedia.search(m[5:len(m)])) != 0 :
-			await message.channel.send("je réponds")
-			await message.channel.send(wikipedia.summary(wikipedia.search(m[5:len(m)])[0]))
+			result = wikipedia.summary(wikipedia.search(m[5:len(m)])[0]))
 		else :
-			await message.channel.send(wikipedia.summary(m[5:len(m)]))
+			result = (wikipedia.summary(m[5:len(m)]))
+		await message.channel.send(result)
 
 	if m.startswith("?iind degré") | m.startswith("?second degré") | m.startswith("?évangile selon saint franck") | m.startswith("?évangile selon st franck") | m.startswith(".deuxième degré") :
 		await message.channel.send("IInd degré chiffré 6 : on double la basse si possible au ténor, et ça descend !")
